1f706566d30a14877305cc28e39aabcb7a36f148
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-05-27 11:24:32 +0200

    Feat: Add UserSerivce - create and list users

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using UserService.Data;
+using UserService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<UserServiceHandler>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ Services/UserServiceHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+using UserService.Models;
+
+namespace UserService.Services;
+
+public class UserServiceHandler
+{
+    public readonly UserDbContext _context;
+
+    public UserServiceHandler(UserDbContext context)
+    {
+        _context = context;
+    }
+    // TODO: Behövs den för validering i BookingService?? Behåll sålänge, 27/5 
+    public async Task<User?> GetUserByIdAsync(Guid userId)
+    {
+        return await _context.Users.FindAsync(userId);
+    }
+
+    //Lista alla användare(för en eventuell admin)
+    public async Task<IEnumerable<User>> GetAllUsersAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task<User> CreateUserAsync(User user)
+    {
+        user.UserId = Guid.NewGuid();
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+}
