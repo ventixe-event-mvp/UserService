427234ad310a3035a98de62d445c14b0d7a66cd6
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-05-27 11:10:34 +0200

    Add project files.

---
+++ Migrations/20250527090326_Init.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Models;
+
+public class User
+{
+    public Guid UserId { get; set; }
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
